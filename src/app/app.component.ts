b2df0558650eabd525b345cbc50b8ab9371898ae
gaderg <roshanggade99@gmail.com>
2024-06-16 08:33:56 +0530

    Added changes for virtual scroll

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,17 @@
-import { Component } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { TableVirtualScrollComponent } from "./angular-material/components/table-virtual-scroll/table-virtual-scroll.component";
+import { HttpClientModule } from '@angular/common/http';
+import { DataSharingService } from './services/data-sharing.service';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
+  providers: [DataSharingService],
+  imports: [RouterOutlet, TableVirtualScrollComponent, HttpClientModule],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class AppComponent {
   title = 'Iw-Sanity-List';
